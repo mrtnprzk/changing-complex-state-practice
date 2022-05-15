078c36c76eb390e6307495b5054fd3d02324f12d
Martin Parizek <marti.parizek@gmail.com>
2022-05-15 13:47:45 +0200

    Spread Oberator added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,25 +18,10 @@ function App() {
     
     const {name, value} = e.target;
 
-    setContact(prevValue =>{
-      if (name === "fName") {
-        return{
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        }
-      } else if (name === "lName") {
-        return{
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        }
-      } else if (name === "email") {
-        return{
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        }
+    setContact( (prevValue) => {
+      return{
+        ...prevValue,
+        [name]: value
       }
     })
   }
